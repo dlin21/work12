33af430c21f5e7616db12aad29f303b740fc2ada
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-26 22:24:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,17 +7,15 @@
 
 int main(int argc, char *argv[]) {
   char buffer[1000];
-  if (argc > 1) strcpy(buffer, argv[1]);
+
+  if (argc > 1) strcpy(buffer,argv[1]);
   else {
     printf("Enter a directory:\n");
     int err = read(STDIN_FILENO, buffer, sizeof(buffer));
-    if (err == -1){
-      printf("error: %s", strerror(errno));
-      return 0;
-    }
+    if (err == -1) printf("error: %s", strerror(errno));
     buffer[strlen(buffer) - 1] = 0;
   }
-    
+  
   DIR *dir;
   struct dirent *d;
   struct stat file;
@@ -28,30 +26,30 @@ int main(int argc, char *argv[]) {
     printf("error: %s", strerror(errno));
     return 0;
   }
-
   while ((d=readdir(dir))!=NULL){
     if(d->d_type == 8) {
       stat(d->d_name, &file);
-      file_size+=file.st_size;
+      file_size += file.st_size;
     }
   }
+    
   printf("Statistics for directory: %s\n", buffer);
   printf("Total Directory Size: %lld Bytes\n", file_size);
 
   rewinddir(dir);
   printf("Directories: \n");
-  while ((d=readdir(dir))!= NULL){
-    if(d->d_type == 4) printf("\t%s\n", d->d_name);
+  while ((d=readdir(dir))!=NULL){
+    if(d->d_type == 4) printf("\t%s\n",d->d_name);
   }
-
   rewinddir(dir);
   printf("Regular Files: \n");
-  while ((d=readdir(dir)) != NULL){
+  while ((d=readdir(dir))!=NULL){
     if(d->d_type == 8) {
       stat(d->d_name, &file);
       file_size += file.st_size;
-      printf("\t%s\n", d->d_name);
+      printf("\t%s\n",d->d_name);
     }
   }
+  
   return 0;
 }
