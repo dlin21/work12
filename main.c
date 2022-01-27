020a42b502a7502a717d6de67ee1521131197726
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-26 22:30:02 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,50 +6,50 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char buffer[1000];
-
-  if (argc > 1) strcpy(buffer,argv[1]);
+  char FILE_PATH[1000];
+  if (argc > 1) strcpy(FILE_PATH,argv[1]);
   else {
     printf("Enter a directory:\n");
-    int err = read(STDIN_FILENO, buffer, sizeof(buffer));
-    if (err == -1) printf("error: %s", strerror(errno));
-    buffer[strlen(buffer) - 1] = 0;
+    int err = read(STDIN_FILENO, FILE_PATH, sizeof(FILE_PATH));
+    if (err == -1) printf("Error %d: %s", errno, strerror(errno));
+    FILE_PATH[strlen(FILE_PATH) - 1] = 0;
   }
   
   DIR *dir;
   struct dirent *d;
-  struct stat file;
-  long long file_size;
+  struct stat stats;
+  long long filesizes;
 
-  dir = opendir(buffer);
+  dir = opendir(FILE_PATH);
   if (!dir) {
-    printf("error: %s", strerror(errno));
+    printf("Error %d: %s", errno, strerror(errno));
     return 0;
   }
+
   while ((d=readdir(dir))!=NULL){
     if(d->d_type == 8) {
-      stat(d->d_name, &file);
-      file_size += file.st_size;
+      stat(d->d_name, &stats);
+      filesizes+=stats.st_size;
     }
   }
-    
-  printf("Statistics for directory: %s\n", buffer);
-  printf("Total Directory Size: %lld Bytes\n", file_size);
+  printf("Statistics for directory: %s\n", FILE_PATH);
+  printf("Total Directory Size: %lld Bytes\n", filesizes);
 
   rewinddir(dir);
   printf("Directories: \n");
   while ((d=readdir(dir))!=NULL){
     if(d->d_type == 4) printf("\t%s\n",d->d_name);
   }
+
   rewinddir(dir);
   printf("Regular Files: \n");
   while ((d=readdir(dir))!=NULL){
     if(d->d_type == 8) {
-      stat(d->d_name, &file);
-      file_size += file.st_size;
+      stat(d->d_name, &stats);
+      filesizes+=stats.st_size;
       printf("\t%s\n",d->d_name);
     }
   }
-  
+
   return 0;
 }
